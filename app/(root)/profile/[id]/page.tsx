38b436f8c35721cd61d5eb5e8c71fdc28dcff417
aryansehrawat13 <sehrawataryan48@gmail.com>
2025-07-11 23:13:32 +0530

    home page UI

---
+++ app/(root)/profile/[id]/page.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const page = async ({params}: ParamsWithSearch) => {
+    const {id} = await params
+  return (
+    <div className='wrapper page'>
+        <Header subHeader='example@gamil.com' title='aryan | JS' userImg='/assets/images/dummy.jpg' />
+        <h1 className='text-2xl font-karla'>User Id: {id}</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ components/DropdownList.tsx
@@ -0,0 +1,32 @@
+'use client';
+import Image from 'next/image';
+import React, { useState } from 'react'
+
+const DropdownList = () => {
+    const [isOpen, setisOpen] = useState(false);
+
+  return (
+    <div className='relative'>
+        <div className='cursor-pointer' onClick={() => setisOpen(!isOpen)}>
+            <div className="filter-trigger">
+                <figure>
+                    <Image src="/assets/icons/hamburger.svg" alt='menu' width={14} height={14} />
+                    Most recent
+                </figure>
+                <Image src="/assets/icons/arrow-down.svg" alt='arrow-down' width={20} height={20} />
+            </div>
+        </div>
+        {isOpen && (
+            <ul className='dropdown'>
+                {['most recent', 'most liked'].map((option) => (
+                    <li key={option} className='list-item'>
+                        {option}
+                    </li>
+                ))}
+            </ul>
+        )}
+    </div>
+  )
+}
+
+export default DropdownList
